4ebed8bdda5833d7fabd4aa6e5196dd96b59aea5
Tibor Ujlaky <tujlaky@gmail.com>
2023-03-24 22:10:18 +0100

    Init

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { serve } from 'https://deno.land/std@0.177.0/http/server.ts';
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+
+const { SLACK_BOT_TOKEN, WEBHOOK_URL, UID } = parse(Deno.args);
+
+serve(async (req: Request) => {
+  const url = new URL(req.url);
+  const secret = url.searchParams.get('secret');
+
+  if (!secret || secret !== SLACK_BOT_TOKEN) {
+    return new Response('Invalid token', {
+      status: 403
+    });
+  }
+
+  await fetch(WEBHOOK_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      text: `<@${UID}> Start here`
+    })
+  });
+
+  return new Response(null, {
+    status: 201
+  });
+});
